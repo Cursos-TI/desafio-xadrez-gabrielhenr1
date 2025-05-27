14246d7a218b3e38b45b0b56299031d7eb072636
gabrielhenr1 <gabriel26254@gmail.com>
2025-05-27 01:18:06 +0000

    versao2

--- xadrez.c
+++ xadrez.c
@@ -8,28 +8,42 @@ int main(){
     printf("1 - Bispo\n");
     printf("2 - Torre\n");
     printf("3 - Rainha\n");
+    printf("4 - Cavalo\n");
     scanf("%d",&escolha);
 
     switch (escolha)
     {
-    case 1:
-        while(i <= 5){
-            printf("Direita\n");
-            printf("Cima\n");
-            i ++;
-        }
-        break;
-    case 2:
-        do{
-            printf("Direita\n");
-            i ++;
-        }while(i <= 5);
-        break;
-    case 3:
-        for(i = 0; i <= 8; i ++){
-            printf("Esquerda\n");
-        }
-    default:
-        break;
+        case 1:
+            while(i <= 5){
+                printf("Direita\n");
+                printf("Cima\n");
+                i ++;
+            }
+            break;
+        case 2:
+            do{
+                printf("Direita\n");
+                i ++;
+            }while(i <= 5);
+            break;
+        case 3:
+            for(i = 0; i <= 8; i ++){
+                printf("Esquerda\n");
+            }
+            break;
+        case 4:
+            i = 0;
+            while (i != 1)
+            {
+                for( int j = 0; j < 2; j ++){
+                    printf("Baixo\n");
+                }
+                i++;
+            }
+            printf("Esquerda");
+            break;
+        default:
+            break;
     }
-}
+
+} 
